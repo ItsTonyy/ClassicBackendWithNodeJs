1dad2c4454c6b9b438f4fdf097071b7f78b2f0d6
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-08-10 00:23:35 -0300

    type 1 and 2 of api add

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const app = express();
 const port = 3000;
+app.listen(port, () => console.log(`backend is running on port ${port}`));
+app.get('/', (req, res) => {
+    console.log('I hit the endpoint', req.method);
+    res.status(200);
+});
+console.log('teste');
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,9 +1,37 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 3000
 
-app.listen(port, () => {
-  console.log(`backend is running on port ${port}`)
+const data = { 
+  name: 'Tony',
+  language: 'Nodejs',
+  framework: 'Express.js'
+}
+
+interface myRequest extends Request {
+  // im doomed
+  status(number: number): number
+  send(message: any): any
+}
+
+app.listen(PORT, () => console.log(`backend is running on PORT ${PORT}`))
+
+// Type 1 - website endpoints, send html when requested 
+
+app.get('/', (req: Request, res: myRequest) => {
+  console.log('default endpoint hitted', req.method)
+  res.status(200)
+  res.send('<h1> This is the homepage <h1/>')
+})  
+
+app.get('/dashboard', (req: Request, res: myRequest) => {
+  console.log('dashboad endpoint hitted', req.method)
+  res.send('<h1> This is the dashboard <h1/>')
 })
 
+// Type 2 - API endpoints, sends data when requested (non-visual)
 
+app.get('/api/data', (req: Request, res: myRequest) => {
+  console.log('sending data...')
+  res.send(data)
+})
