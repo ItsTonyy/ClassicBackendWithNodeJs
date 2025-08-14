81e8c091c36443ab7d7239df259b9606f1599aa0
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-08-14 00:19:35 -0300

    improved html sent from the endpoints

--- server.ts
+++ server.ts
@@ -4,12 +4,14 @@ import type {Request, Response} from 'express'
 const app = express()
 const PORT = 3000
 
+interface requestBody {
+  name: string,
+}
+
 //middleware
 app.use(express.json())
 
-const data = { 
-  name: 'Tony',
-}
+const data = ['Tony']
 
 app.listen(PORT, () => console.log(`backend is running on PORT ${PORT}`))
 
@@ -18,12 +20,26 @@ app.listen(PORT, () => console.log(`backend is running on PORT ${PORT}`))
 app.get('/', (req: Request, res: Response) => {
   console.log('default endpoint hitted', req.method)
   res.status(200)
-  res.send('<body style="background-color: pink"><h1> FINALMENTE PORRA <h1/></body>')
+  res.send(`
+    <body style="background-color: black; color:white;">
+      <a href='/dashboard' style="color:white;"> Dashboard </a>
+      <h1>Home Endpoint
+        <h2>${JSON.stringify(data)}</h2>
+       </h1>
+    </body>
+  `)
 })  
 
 app.get('/dashboard', (req: Request, res: Response) => {
   console.log('dashboad endpoint hitted', req.method)
-  res.send('<h1> This is the dashboard <h1/>')
+   res.send(`
+    <body style="background-color: black; color:white;">
+      <a href='/' style="color:white;"> Home </a>
+      <h1>Dashboard Endpoint
+        <h2>${JSON.stringify(data)}</h2>
+       </h1>
+    </body>
+  `)
 })
 
 // Type 2 - API endpoints, sends data when requested (non-visual)
@@ -34,6 +50,14 @@ app.get('/api/data', (req: Request, res: Response) => {
 })
 
 app.post('/api/data', (req: Request, res: Response) => {
-  const reqBody = req.body
+  const reqBody: requestBody = req.body
+  console.log(`pushing to the array: ${reqBody.name}`)
+  data.push(reqBody.name)
   res.status(201)
+})
+
+app.delete('/api/data', (req: Request, res: Response) => {
+  data.pop()
+  console.log('deleted the last element from the data array')
+  res.status(203)
 })
