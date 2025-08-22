2d4ab41504d4bcfc6aaf2454a416e93047d01021
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-08-22 19:59:36 -0300

    adding the pre completed frontend

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,6 @@ import express from 'express'
 import type {Request, Response} from 'express'
 
 const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`backend is running on PORT ${PORT}`))
