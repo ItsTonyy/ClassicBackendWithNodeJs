0244da4a3be28b1f8929cdf61a2f06daecb6ff79
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-08-22 23:19:25 -0300

    finished main login of server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import express from 'express'
 import type {Request, Response} from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+const __fileName = fileURLToPath(import.meta.url)
+
+const __dirname = dirname(__fileName)
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`backend is running on PORT ${PORT}`))
